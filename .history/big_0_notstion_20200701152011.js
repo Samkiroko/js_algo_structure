8e6f52dd336b4243514ef42003efa961dd4500a8
Samkiroko <kirokodev@gmail.com>
2020-07-01 15:21:39 +0300

    big o notion

---
+++ .history/big_0_notstion_20200701152011.js
@@ -0,0 +1,12 @@
+// Suppose we want to write a function that calculates the sum of all numbers
+// form 1 up to (and including) some number n.
+
+function addUpTo(n) {
+  let total = 0;
+  for (let i = 1; i <= n; i++) {
+    total += i;
+  }
+  return total;
+}
+
+console.log(addUpTo(6));
