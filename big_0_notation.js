ddaa54b05962c9311150528c6fe563d63914afd7
Samkiroko <kirokodev@gmail.com>
2020-07-01 17:44:47 +0300

    big o notation O(n2)

--- big_0_notation.js
+++ big_0_notation.js
@@ -8,5 +8,32 @@ function addUpTo(n) {
   }
   return total;
 }
+console.log(addUpTo(1000000000));
 
-console.log(addUpTo(6));
+// for the above operation the big o = O(n) because  the n operation grows with
+// the input
+
+// another way of doing it
+function addUpTo2(n) {
+  return (n * (n + 1)) / 2;
+}
+
+console.log(addUpTo2(16));
+
+// for the above function  = O(1) no matter the input n doest grow
+
+// which on is better
+/* faster?*/
+/* less memory-intensive?*/
+/* More readable?*/
+
+function printAllPairs(n) {
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j < n; j++) {
+      console.log(i, j);
+    }
+  }
+}
+console.log(printAllPairs(10));
+
+// the above notation = O(n2)
