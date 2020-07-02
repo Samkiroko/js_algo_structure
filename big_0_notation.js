7225da3d78c62f45efc44914e88faefd7f972d4d
Samkiroko <kirokodev@gmail.com>
2020-07-02 12:04:35 +0300

    Code challage & learming how to solve problems

--- big_0_notation.js
+++ big_0_notation.js
@@ -34,6 +34,54 @@ function printAllPairs(n) {
     }
   }
 }
-console.log(printAllPairs(10));
+console.log(printAllPairs(2));
 
 // the above notation = O(n2)
+
+/*function that return the count of each character */
+
+function charCount(str) {
+  let obj = {};
+  for (let i = 0; i < str.length; i++) {
+    let char = str[i].toLowerCase();
+    if (/[a-z0-9]/.test(char)) {
+      if (obj[char] > 0) {
+        obj[char]++;
+      } else {
+        obj[char] = 1;
+      }
+    }
+  }
+  return obj;
+}
+console.log(charCount("happy coding"));
+
+/*
+refactoring the above solution
+*/
+
+function charCount1(str) {
+  let obj = {};
+  for (let char1 of str) {
+    if (isAlphaNumeric(char1)) {
+      char1 = char1.toLowerCase();
+      obj[char1] = ++obj[char1] || 1;
+    }
+  }
+  return obj;
+}
+
+function isAlphaNumeric(char) {
+  let code = char.charCodeAt(0);
+  if (
+    !(code > 47 && code < 58) && // numeric (0-9)
+    !(code > 64 && code < 91) && //upper alpha(A-Z)
+    !(code > 96 && code < 123)
+  ) {
+    // lower alpha (a-z)
+    return false;
+  }
+  return true;
+}
+
+console.log(charCount1("happy coding happy"));
